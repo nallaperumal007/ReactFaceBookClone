f6e4cc18af7fb16bd0aae88a09609b73502d9ba1
nallaperumal007 <139489689+nallaperumal007@users.noreply.github.com>
2023-08-11 09:40:33 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,15 @@ import firebase from 'firebase';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAyocn5GHBV06TISHH8Oq6KsltCjVKhAbA",
-  authDomain: "facebookclone-bf7ea.firebaseapp.com",
-  projectId: "facebookclone-bf7ea",
-  storageBucket: "facebookclone-bf7ea.appspot.com",
-  messagingSenderId: "1035593790770",
-  appId: "1:1035593790770:web:306050543a6a7349b5932b",
-  measurementId: "G-2SBQ0GXDHG"
+  apiKey: "AIzaSyAwWAqjXD5u-hujA2rIUZYZmmtjO1e_i_8",
+  authDomain: "facebook-ccbe0.firebaseapp.com",
+  projectId: "facebook-ccbe0",
+  storageBucket: "facebook-ccbe0.appspot.com",
+  messagingSenderId: "831881220903",
+  appId: "1:831881220903:web:87dc4c9daf1493603bcf02",
+  measurementId: "G-HDVJMH8T9L"
 };
 
 //connecting frontend to backend
